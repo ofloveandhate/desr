acee6629a326cd7ca71f1a200d043845d936ba49
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2026-02-23 13:44:25 +0100

    added import statements
    so can actually import the submodules

---
+++ desr/__init__.py
@@ -0,0 +1,7 @@
+from . import ode_system
+from . import diophantine
+from . import matrix_normal_forms
+from . import chemical_reaction_network
+from . import ode_translation
+from . import sympy_helper
+from . import tex_tools
