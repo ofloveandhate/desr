7bf8cacf5927a470f4a370de2b3ca485159d61c4
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2026-03-24 10:39:40 +0100

    updated linear compartment model example code
    including sources in comments

--- examples/linear_compartment.py
+++ examples/linear_compartment.py
@@ -1,3 +1,17 @@
+# implements example from 
+#
+# R. P. Weissburg, P. W. Berman, J. L. Cleland, D. Eastman, F. Farina, S. Frie, A. Lim, J. Mordenti,
+# M. R. Peterson, K. Yim, and M. F. Powell, Characterization of the MN gp120 HIV-1 vaccine:
+# Antigen binding to alum, Pharmaceutical Research, 12 (1995), pp. 1439–1446, 
+# https://doi.org/10.1023/A:1016266916893.
+#
+# and
+# 
+# example 13.6 in 
+# J. DiStefano, Dynamic Systems Biology Modeling and Simulation, Elsevier Science, 2015, 
+# https://books.google.co.uk/books?id=kkkhmwEACAAJ
+
+
 import sympy
 from desr.matrix_normal_forms import smf
 from desr.ode_system import ODESystem
@@ -21,6 +35,7 @@ def run_symmetry_reduction(sys):
     print('\nScaling matrix:')
     print(translation.scaling_matrix.__repr__())
 
+    print('Dimension of scaling action: ', translation.r)
     # Print invariants
     print('Invariants: ', translation.invariants())
     print('Substitutions: ', translation.translate_parameter_substitutions(system=sys))
@@ -38,27 +53,29 @@ def run_symmetry_reduction(sys):
 print('\n\nA\n\n')
 tex_a =  \
 r'''
-\frac{dx_1}{dt} &=-(a_1+a_21) x_1+u_1\\
-\frac{dx_2}{dt} &= a_21 - a_2 x_2
+\frac{dz_1}{dt} &= -a_1 z_1 + a_12 z_2\\
+\frac{dz_2}{dt} &= -(a_2 + a_12) z_2 + u_2
 '''
 
-
 system_a = ODESystem.from_tex(tex_a)
-# system_a.add_constraint(lhs='y_2',rhs='x_2')
-
-
+# system_a.add_constraint(lhs='w_1',rhs='z_1')
+# desr cannot have constraints on non-constant variables, so commented out
 translation_a = run_symmetry_reduction(system_a)
+
+
 
 
 print('\n\nB\n\n')
 tex_b =  \
 r'''
-\frac{dx_1}{dt} &=-b_11 x_1+u_1\\
-\frac{dx_2}{dt} &= a_21 - a_2 x_2
+\frac{dz_1}{dt} &= -a_1 z_1 + a_12 z_2\\
+\frac{dz_2}{dt} &= -b_22 z_2 + u_2
 '''
 
 system_b = ODESystem.from_tex(tex_b)
-# system_a.add_constraint(lhs='y_2',rhs='x_2')
+# system_b.add_constraint(lhs='w_1',rhs='z_1')
+# desr cannot have constraints on non-constant variables, so commented out
+
 
 
 translation_b = run_symmetry_reduction(system_b)
@@ -67,12 +84,14 @@ translation_b = run_symmetry_reduction(system_b)
 print('\n\nC\n\n')
 tex_c =  \
 r'''
-\frac{dx_1}{dt} &=-b_11 x_1+u_1\\
-\frac{dx_2}{dt} &= a_21 - a_2 x_2
+\frac{dz_1}{dt} &= -a_1 z_1 + a_12 z_2\\
+\frac{dz_2}{dt} &= -b_22 z_2 + u_2
 '''
 
 system_c = ODESystem.from_tex(tex_c)
-system_c.add_constraint(lhs='b_11',rhs='a_1 + a_21')
+# system_c.add_constraint(lhs='w_1',rhs='z_1')
+# desr cannot have constraints on non-constant variables, so commented out
+system_c.add_constraint(lhs='b_22',rhs='a_2 + a_12')
 
 
 translation_c = run_symmetry_reduction(system_c)
